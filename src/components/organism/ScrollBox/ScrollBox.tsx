f8d7930edaf997413a6ffdfe6c6a4f8b560e4c46
Luis Molina <luismateo2416@gmail.com>
2022-05-08 13:27:51 +0200

    ScrollBox organism component created

---
+++ src/components/organism/ScrollBox/ScrollBox.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "../../molecules/Card/Card";
+
+import "./ScrollBox.css";
+
+const ScrollBox = () => {
+  return (
+    <article className="scrollBoxContainer">
+      <Card title="Example" />
+    </article>
+  );
+};
+
+export default ScrollBox;
